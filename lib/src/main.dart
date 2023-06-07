6e45134bd15d624820d5706507d7156454bd7fb9
Muhammad Rafi Andeo Praja <rafiandeo26@gmail.com>
2023-06-08 00:02:19 +0700

    Init Firebase

--- lib/src/main.dart
+++ lib/src/main.dart
@@ -1,7 +1,11 @@
 import 'package:flutter/material.dart';
+import 'package:tubes_klip/src/firebase_options.dart';
 import 'package:tubes_klip/pages/LoginPage.dart';
+import 'package:firebase_core/firebase_core.dart';
 
 void main() {
+  WidgetsFlutterBinding.ensureInitialized();
+  Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
 }
 
--- test/widget_test.dart
+++ test/widget_test.dart
@@ -8,7 +8,7 @@
 import 'package:flutter/material.dart';
 import 'package:flutter_test/flutter_test.dart';
 
-import 'package:tubes_klip/main.dart';
+import 'package:tubes_klip/src/main.dart';
 
 void main() {
   testWidgets('Counter increments smoke test', (WidgetTester tester) async {
