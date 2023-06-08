e0d655bc2a406fb6a6fd877e4bcccafbd62565f2
Muhammad Rafi Andeo Praja <rafiandeo26@gmail.com>
2023-06-08 22:41:10 +0700

    Init Firebase Database Real Time

--- lib/src/main.dart
+++ lib/src/main.dart
@@ -2,11 +2,22 @@ import 'package:flutter/material.dart';
 import 'package:tubes_klip/src/firebase_options.dart';
 import 'package:tubes_klip/pages/LoginPage.dart';
 import 'package:firebase_core/firebase_core.dart';
+import 'package:firebase_database/firebase_database.dart';
 
-void main() {
+void main() async {
   WidgetsFlutterBinding.ensureInitialized();
-  Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
+  await Firebase.initializeApp(options: DefaultFirebaseOptions.currentPlatform);
   runApp(const MyApp());
+  FirebaseDatabase database = FirebaseDatabase.instance;
+  database.databaseURL =
+      'https://klip-56f41-default-rtdb.asia-southeast1.firebasedatabase.app/';
+  final userRef = database.ref().child('users');
+  await userRef.push().set({
+    'name': 'Rafi',
+    'email': 'rafiandeo@gmail.com',
+    'password': '123456',
+    'roles': 'admin',
+  });
 }
 
 class MyApp extends StatelessWidget {
